144f7b74a143e5d81de22179e136f1b4f6efb2ff
Gazinur Galimzyanov <minecraftmaus@yandex.ru>
2018-06-03 19:04:40 +0300

    added makeTurn

--- tic_tac_toe_back/src/main/java/ru/bagautdinov/utils/CommadnsHandler.java
+++ tic_tac_toe_back/src/main/java/ru/bagautdinov/utils/CommadnsHandler.java
@@ -12,5 +12,25 @@ public class CommadnsHandler {
         } else if (commands[0].equals("turn")) {
             makeTurn(game, commands[1], commands[2]);
         }
+    } 
+    void makeTurn(Player player,Game game,int a,int b){
+        int currentPlayer=game.getCounter()%2;
+        if ((player.equals(game.getP1())&&currentPlayer==0)||(player.equals(game.getP2())&&currentPlayer==1)){
+            if(game.getField[a][b]==' '){
+                if(currentPlayer==0){
+                    game.getField[a][b]='X';
+                    if(checkWinner(game,'X')){
+
+                    }
+                }
+                else{
+                    game.getField[a][b]='O';
+                    checkWinner(game,'X');
+                }
+            }
+        }
+    }
+
+    private boolean checkWinner(Game game, char x) {
     }
 }
