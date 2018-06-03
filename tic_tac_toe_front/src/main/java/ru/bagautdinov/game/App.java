f85bab7bed272844665bf74560dc8671f1dfd3ab
D0ct0r_Zl0 <doctorzlo1177@gmail.com>
2018-06-03 21:35:20 +0300

    TextField

--- tic_tac_toe_front/src/main/java/ru/bagautdinov/game/App.java
+++ tic_tac_toe_front/src/main/java/ru/bagautdinov/game/App.java
@@ -20,7 +20,7 @@ public class App  extends Application {
     public void start(Stage primaryStage) throws Exception {
         menuStage = new Stage();
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
             menuStage.setTitle("Tic Tac Toe Menu");
             menuStage.setScene(new Scene(root));
             menuStage.initModality(Modality.WINDOW_MODAL);
@@ -30,7 +30,7 @@ public class App  extends Application {
 
         gameStage = new Stage();
         try {
-            Parent gameRoot = FXMLLoader.load(getClass().getResource("tictactoe.fxml"));
+            Parent gameRoot = FXMLLoader.load(getClass().getResource("/tictactoe.fxml"));
             gameStage.setTitle("Tic Tac Toe Game");
             gameStage.setScene(new Scene(gameRoot));
             menuStage.initModality(Modality.WINDOW_MODAL);
