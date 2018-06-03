8cf03662ae429050a1abbac27f3b00e088173e22
D0ct0r_Zl0 <doctorzlo1177@gmail.com>
2018-06-03 20:38:57 +0300

    GameController

---
+++ tic_tac_toe_front/src/main/java/ru/bagautdinov/game/GameController.java
@@ -0,0 +1,106 @@
+package ru.bagautdinov.game;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+import java.awt.event.ActionEvent;
+
+public class GameController {
+
+    private String playerChar;
+
+    public String getPlayerChar() {
+        return playerChar;
+    }
+
+    public void setPlayerChar(String playerChar) {
+        this.playerChar = playerChar;
+    }
+
+    @FXML
+    public StackPane cell_0_0;
+    @FXML
+    public StackPane cell_0_1;
+    @FXML
+    public StackPane cell_0_2;
+    @FXML
+    public StackPane cell_1_0;
+    @FXML
+    public StackPane cell_1_1;
+    @FXML
+    public StackPane cell_1_2;
+    @FXML
+    public StackPane cell_2_0;
+    @FXML
+    public StackPane cell_2_1;
+    @FXML
+    public StackPane cell_2_2;
+
+    @FXML
+    public Text text_0_0;
+    @FXML
+    public Text text_0_1;
+    @FXML
+    public Text text_0_2;
+    @FXML
+    public Text text_1_0;
+    @FXML
+    public Text text_1_1;
+    @FXML
+    public Text text_1_2;
+    @FXML
+    public Text text_2_0;
+    @FXML
+    public Text text_2_1;
+    @FXML
+    public Text text_2_2;
+
+    public void paneCliced(ActionEvent event) {
+        if (event.getSource() == cell_0_0) {
+            if (text_0_0.getText().equals("")) {
+                text_0_0.setText(playerChar);
+            }
+        }
+        if (event.getSource() == cell_0_1) {
+            if (text_0_1.getText().equals("")) {
+                text_0_1.setText(playerChar);
+            }
+        }
+        if (event.getSource() == cell_0_2) {
+            if (text_0_2.getText().equals("")) {
+                text_0_2.setText(playerChar);
+            }
+        }
+        if (event.getSource() == cell_1_0) {
+            if (text_1_0.getText().equals("")) {
+                text_1_0.setText(playerChar);
+            }
+        }
+        if (event.getSource() == cell_1_1) {
+            if (text_1_1.getText().equals("")) {
+                text_1_1.setText(playerChar);
+            }
+        }
+        if (event.getSource() == cell_1_2) {
+            if (text_1_2.getText().equals("")) {
+                text_1_2.setText(playerChar);
+            }
+        }
+        if (event.getSource() == cell_2_0) {
+            if (text_2_0.getText().equals("")) {
+                text_2_0.setText(playerChar);
+            }
+        }
+        if (event.getSource() == cell_2_1) {
+            if (text_2_1.getText().equals("")) {
+                text_2_1.setText(playerChar);
+            }
+        }
+        if (event.getSource() == cell_2_2) {
+            if (text_2_2.getText().equals("")) {
+                text_2_2.setText(playerChar);
+            }
+        }
+    }
+}
