3750d2aeead6b6e24c22163d4ef8941bf035d3b2
D0ct0r_Zl0 <doctorzlo1177@gmail.com>
2018-06-03 19:57:59 +0300

    client

--- tic_tac_toe_back/src/main/java/ru/bagautdinov/ServerEndPoint.java
+++ tic_tac_toe_back/src/main/java/ru/bagautdinov/ServerEndPoint.java
@@ -1,7 +1,10 @@
 package ru.bagautdinov;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import ru.bagautdinov.models.Game;
 import ru.bagautdinov.models.Player;
+import ru.bagautdinov.utils.CommadnsHandler;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
@@ -14,8 +17,12 @@ import java.util.Set;
 import static java.lang.String.format;
 
 
+@Component
 @ServerEndpoint(value = "/server", encoders = {MessageEncoder.class}, decoders = {MessageDecoder.class})
 public class ServerEndPoint {
+
+    @Autowired
+    public CommadnsHandler commadnsHandler;
 
     static Set<Session> detailUsers = Collections.synchronizedSet(new HashSet<Session>());
 
@@ -28,12 +35,12 @@ public class ServerEndPoint {
     @OnMessage
     public void handleMessage(Message incomingMessage, Session session) throws IOException, EncodeException {
         Game currentGame = (Game) session.getUserProperties().get("currentGame");
-        Player player = (Game) session.getUserProperties().get("player");
+        Player player = (Player) session.getUserProperties().get("player");
+
+        commadnsHandler.proceedCommand(incomingMessage.getMessage(), currentGame, player);
+
+
         Message outcomingMessageData = new Message();
-
-        proceedCommand(incomingMessage.getMessage(), currentGame, player);
-
-
 
         if (username == null){
             session.getUserProperties().put("username", incomingMessage.getUsername());
